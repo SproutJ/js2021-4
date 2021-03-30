987201524f3d54f051a342c966be437c4399e604
SproutJ <sprouttree341@gmail.com>
2021-03-30 17:32:45 +0900

    첫 번째 커밋(0330)

--- hello.js
+++ hello.js
@@ -20,7 +20,7 @@ if(date.getHours() < 12){
     console.log("오전!");
 }
 console.log("오후!");*/
-
+/*
 console.log("예제");
 
 let date = new Date();
@@ -34,4 +34,40 @@ if(hours<11){
     } else{
         console.log("저녁 먹을 시간.");
     }
+}*/
+let date = new Date();
+
+console.log(date.getMonth());
+
+switch(date.getMonth()){
+    case 12:
+    case 1:
+    case 2:
+        console.log("겨울");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log("봄");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log("여름");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log("가을");
+        break;
+    default:
+        console.log("?????");
+        break;
 }
+// let test = 777;
+let test;
+// test = test ? test : "초기화";
+// console.log(test);
+
+test = test || "초기화!!"
+console.log(test);
