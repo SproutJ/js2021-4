022648f199e148f5eafbe43c1fded62f110c5368
SproutJ <sprouttree341@gmail.com>
2021-04-13 12:40:17 +0900

    첫번째 커밋(0413)

---
+++ ch5.js
@@ -0,0 +1,119 @@
+//  무명 함수
+// let foo = function(){
+//     console.log("첫줄");
+//     console.log("둘째줄");
+// }
+
+
+// foo();
+// console.log(foo);*/
+
+//  선언적 함수
+// function foo(){
+//     console.log("즐거운 하루");
+// }
+
+// foo();
+// console.log(foo);
+
+// 화살표 함수. ES6 문법.
+// let foo = () =>{
+//     console.log("신나는 하루");
+// }
+
+// foo();
+// console.log(foo);
+
+// function power(x){
+//     return x * x;
+// }
+
+// console.log(power(10));
+// console.log(power(20));
+
+// function multiply(x, y){
+//     return x * y;
+// }
+
+// console.log(multiply(10, 20));
+
+// function print(x){
+//     console.log(`"${x}"이라고 말했습니다.`);
+// }
+
+// print(5);
+
+// function sum(min, max){
+//     let output=0;
+//     for(let i = min;i<=max;i++){
+//         output+=i;
+//     }
+//     console.log(output);
+// }
+
+// sum(1,100);
+
+// function multi(min, max){
+//     let output=1;
+//     for(let i = min;i<=max;i++){
+//         output*=i;
+//     }
+//     console.log(output);
+// }
+
+// multi(1,20);
+
+// function print(name,count){
+//     if(!count){
+//         count=1;
+//     }
+//     console.log(`${name}이 ${count}개 있습니다.`);
+// }
+
+// print("사과",10);
+// print("사과");
+
+// function print(name,count){
+//     count=count||"미입력";
+//     console.log(`${name}이 ${count}개 있습니다.`);
+// }
+
+// print("사과",10);
+// print("사과");
+
+// function callTenTimes(foo){
+//     for(let i=0;i<10;i++){
+//         foo();
+//     }
+// }
+
+// callTenTimes(function() {
+//     console.log("함수 호출")
+// })
+
+// let inputA="52";
+// let inputB="52.273";
+// let inputC="1401동";
+// let inputD="1A동입니다.";
+
+// console.log(parseInt(inputA));
+// console.log(parseFloat(inputA));
+
+// console.log(parseInt(inputB));
+// console.log(parseFloat(inputB));
+
+// console.log(parseInt(inputC)); // 숫자형태만 변환
+// console.log(parseInt(inputD));
+
+// console.log(Number(inputA));
+// console.log(Number(inputB));
+// console.log(Number(inputC));
+// console.log(Number(inputD));
+
+// setTimeout(function(){
+//     console.log("1초지남!");
+// }, 1000);
+
+setInterval(() => {
+    console.log("1초 지남!")
+}, 1000);
